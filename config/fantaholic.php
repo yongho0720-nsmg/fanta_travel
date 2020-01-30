9a9df7bedab4d3897df50f52c7d2334ce0ee9c78
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-01-30 19:56:46 +0900

    앱설정파일 변경

--- config/fantaholic.php
+++ config/fantaholic.php
@@ -3,7 +3,7 @@ return[
 // App
     'version'=>[
         'and'=>[
-            'app_ver' => '1.1.1',   // 앱 최신 버전
+            'app_ver' => '1.0.0',   // 앱 최신 버전
             'forced_update' => 'auto',   // 앱 강제 업데이트 여부 (forced / auto)
             'inspection'    =>  false
         ],
