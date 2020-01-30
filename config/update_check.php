aa083a5e65735cedd59baa575c8411b79779046d
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-01-30 20:01:01 +0900

    앱설정파일 변경

--- config/update_check.php
+++ config/update_check.php
@@ -57,7 +57,22 @@ return[
                 'inspection'    =>  false
             ]
         ]
+    ],
+    'fantaholic' =>[
+        'version'=>[
+            'and'=>[
+                'app_ver' => '1.0.0',   // 앱 최신 버전
+                'forced_update' => 'auto',   // 앱 강제 업데이트 여부 (forced / auto)
+                'inspection'    =>  false
+            ],
+            'ios'=>[
+                'app_ver' => '1.0.1',   // 앱 최신 버전
+                'forced_update' => 'forced',   // 앱 강제 업데이트 여부 (forced / auto)
+                'inspection'    =>  false
+            ]
+        ]
     ]
+
 
 
 
