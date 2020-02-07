b5384918855a4a18b43b91362209371674b4a072
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-02-07 18:06:29 +0900

    지연시간 추가

--- app/Console/Commands/CrawlerAllCommand.php
+++ app/Console/Commands/CrawlerAllCommand.php
@@ -43,6 +43,8 @@ class CrawlerAllCommand extends Command
             $cnt += $channel::$successCnt;
             $crawler->finaled_at = date('Y-m-d H:i:s');
             $crawler->save();
+          
+            sleep(10);
         }
 
         //게시물 알림 => 새 게시물이 있는 경우 푸쉬
