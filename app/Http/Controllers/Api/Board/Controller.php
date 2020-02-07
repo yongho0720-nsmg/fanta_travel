7fd75e01b251783cc0ef1b33620d1713064b0864
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-02-07 17:16:11 +0900

    게시물 새로고침 - 리턴 url(원본주소) 변경

--- app/Http/Controllers/Api/Board/Controller.php
+++ app/Http/Controllers/Api/Board/Controller.php
@@ -1061,7 +1061,13 @@ class Controller extends baseController
           $result['board']->xpath_ver = config('xpath')[$result['board']->type]['version'];
 
         }
-        $result['board']->url = config('xpath')[$result['board']->type]['url'].$result['board']->sns_account."/status/".$result['board']->post;
+        
+        if($result['board']->type == "twitter"){
+          $result['board']->url = config('xpath')[$result['board']->type]['url'].$result['board']->sns_account."/status/".$result['board']->post;
+        }else{
+          $result['board']->url = config('xpath')[$result['board']->type]['url'].$result['board']->post;
+        }
+
         return $this->response->set_response(0, $result);
 
     }
