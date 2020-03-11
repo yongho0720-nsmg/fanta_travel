5a522768f8d1c49c1ccce69ade38a79f3f1e4ba6
kimseamhee <msmhsi@nsmg21.com>
2020-03-11 16:42:37 +0900

    뉴스 api 예외 처리

--- app/Lib/Channel/News.php
+++ app/Lib/Channel/News.php
@@ -14,7 +14,7 @@ class News extends ChannelAbstractClass
 
     private $artistsId;
     private $channelType = 'news';
-    private $channelImagePath = 'images/news/thumbnail/';
+    private $channelImagePath = 'images/news/thumbnail';
 
     public function __construct($artistsId){
         $this->artistsId = $artistsId;
@@ -39,12 +39,12 @@ class News extends ChannelAbstractClass
     public function getChannelContents()
     {
         $artist_id = $this->artistsId;
-        $artist_id = $artist_id;
+//        $artist_id = 2;
         $names = \DB::table('artists')->where('id',$artist_id)->get();
         $client_id = "QI4CBOw2COVcXoMmVb0_";
         $client_secret = "XRgjR9vD0M";
         $encText = urlencode($names[0]->name);
-        $url = "https://openapi.naver.com/v1/search/news.json?query=".$encText."&display=1"; // json 결과
+        $url = "https://openapi.naver.com/v1/search/news.json?query=".$encText."&display=1&sort=sim"; // json 결과
 
         $is_post = false;
         $ch = curl_init();
@@ -74,59 +74,76 @@ class News extends ChannelAbstractClass
                 $params['app'] = 'fantaholic';
             }
             $cnt = 0;
+            //dd($array_data['items']);
             foreach ($array_data['items'] as $item) {
                 $dupleChk = $this->isValidation($item);
-
                 if ($dupleChk > 0) {
                     break;
                 }
-                $document = [
-                    'artists_id' => $artist_id,
-                    'app' => env('APP_NAME'),
-                    'type' => $this->channelType,
-                    'post' => preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['originallink']),
-                    'post_type' => 'image',
-                    'title' => strip_tags($item['title']),
-                    'contents' => strip_tags($item['description']),
-                    'recorded_at' => strftime("%Y-%m-%d %H:%M:%S", strtotime($item['pubDate'])),
-                    'state' => 0,
-                ];
+//                $text = $item['description'];
+//                dd($names[0]->name);
+//                $reg = preg_match_all('/'. $names[0]->name . '/', $text, $matches);
+//                dd($matches , $item);
+                $search = 'naver';
+                if(strpos($item['link'], $search)) {
+                    $document = [
+                        'artists_id' => $artist_id,
+                        'app' => env('APP_NAME'),
+                        'type' => $this->channelType,
+                        'post' => preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['link']),
+                        'post_type' => 'image',
+                        'title' => strip_tags($item['title']),
+                        'contents' => strip_tags($item['description']),
+                        'recorded_at' => strftime("%Y-%m-%d %H:%M:%S", strtotime($item['pubDate'])),
+                        'state' => 0,
+                    ];
 
 
-                $html = $this->file_get_contents_curl(preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['originallink']));
+                    $html = $this->file_get_contents_curl(preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['link']));
 
-                $doc = new \DOMDocument();
-                @$doc->loadHTML($html);
+                    $doc = new \DOMDocument();
+                    @$doc->loadHTML($html);
 
-                $metas = $doc->getElementsByTagName('meta');
+                    $metas = $doc->getElementsByTagName('meta');
 
-                $img_url = "";
-                for ($i = 0; $i < $metas->length; $i++)
-                {
-                    $meta = $metas->item($i);
-                    if($meta->getAttribute('property') == 'og:image')
-                        $img_url = $meta->getAttribute('content');
+                    $img_url = "";
+                    for ($i = 0; $i < $metas->length; $i++)
+                    {
+                        $meta = $metas->item($i);
+                        if($meta->getAttribute('property') == 'og:image') {
+
+                            $img_url = $meta->getAttribute('content');
+
+                            // file save
+                            if($img_url !== null) {
+                                $util = new Util();
+                                $resized_image = $util->AzureUploadImage($img_url, $this->channelImagePath);
+                                //dd($resized_image['fileName']);
+                                if($resized_image['fileName'] !== null) {
+                                    $document['thumbnail_url'] = '/' . $this->channelImagePath . '/' . $resized_image['fileName'];
+                                    $document['thumbnail_w'] = $resized_image['width'];
+                                    $document['thumbnail_h'] = $resized_image['height'];
+                                    $document['ori_thumbnail'] = preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $img_url);
+                                    $document['data'] = array(['image' => $document['thumbnail_url']]);
+                                    $document['ori_data'] = array($document['thumbnail_url']);
+                                }
+
+                            }
+                        }
+                    }
+
+
+
+                    $board = Board::create($document);
                 }
-                // file save
-                if($img_url !== null) {
-                    $util = new Util();
-                    $resized_image = $util->AzureUploadImage($img_url, $this->channelImagePath);
-                    if($resized_image['fileName'] !== null)
-                    $document['thumbnail_url'] = '/' . $this->channelImagePath . '/' . $resized_image['fileName'];
-                    $document['thumbnail_w'] = $resized_image['width'];
-                    $document['thumbnail_h'] = $resized_image['height'];
-                    $document['ori_thumbnail'] = preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $img_url);
-                    $document['data'] = array(['image' => $document['thumbnail_url']]);
-                    $document['ori_data'] = array($document['thumbnail_url']);
-                }
-                $board = Board::create($document);
             } //for문
         }
     }
 
     private function parsingPost( $channelModa): string
     {
-        return $channelModa['originallink'];
+        $link = str_replace('https', 'http', $channelModa['link']);
+        return $link;
     }
 
     protected function setDataFormatting($channelMode)
@@ -206,18 +223,20 @@ class News extends ChannelAbstractClass
 
     protected function isValidation( $channelModel)
     {
-        return Board::where('post', '=', $this->parsingPost($channelModel))->count();
+        $chk = Board::where('post', '=', $this->parsingPost($channelModel))->count();
+        return $chk;
         // TODO: Implement isValidation() method.
     }
 
     public function getChannelContentsAll()
     {
         $artist_id = $this->artistsId;
+//        $artist_id = 2;
         $names = \DB::table('artists')->where('id',$artist_id)->get();
         $client_id = "QI4CBOw2COVcXoMmVb0_";
         $client_secret = "XRgjR9vD0M";
         $encText = urlencode($names[0]->name);
-        $url = "https://openapi.naver.com/v1/search/news.json?query=".$encText; // json 결과
+        $url = "https://openapi.naver.com/v1/search/news.json?query=".$encText."&display=1"; // json 결과
 
         $is_post = false;
         $ch = curl_init();
@@ -237,7 +256,7 @@ class News extends ChannelAbstractClass
 
         if($status_code == 200) {
             $array_data = json_decode($response, true);
-//            print_r($array_data['items']);
+//            dd($array_data['items']);
 
             $user = env('APP_NAME');
 
@@ -246,54 +265,62 @@ class News extends ChannelAbstractClass
             } else {
                 $params['app'] = 'fantaholic';
             }
+            $cnt = 0;
+            //dd($array_data['items']);
             foreach ($array_data['items'] as $item) {
-                $document = [
-                    'artists_id' => $artist_id,
-                    'app' => env('APP_NAME'),
-                    'type' => $this->channelType,
-                    'post' => preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['originallink']),
-                    'post_type' => 'image',
-                    'title' => $item['title'],
-                    'contents' => $item['description'],
-                    'recorded_at' => strftime("%Y-%m-%d %H:%M:%S", strtotime($item['pubDate'])),
-                    'state' => 1,
-                ];
+                $dupleChk = $this->isValidation($item);
+                $search = 'naver';
+                if ($dupleChk > 0) {
+                    break;
+                }
+                if(strpos($item['link'], $search)) {
+                    $document = [
+                        'artists_id' => $artist_id,
+                        'app' => env('APP_NAME'),
+                        'type' => $this->channelType,
+                        'post' => preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['link']),
+                        'post_type' => 'image',
+                        'title' => strip_tags($item['title']),
+                        'contents' => strip_tags($item['description']),
+                        'recorded_at' => strftime("%Y-%m-%d %H:%M:%S", strtotime($item['pubDate'])),
+                        'state' => 0,
+                    ];
+
+                    $html = $this->file_get_contents_curl(preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $item['link']));
+
+                    $doc = new \DOMDocument();
+                    @$doc->loadHTML($html);
+
+                    $metas = $doc->getElementsByTagName('meta');
+
+                    $img_url = "";
+                    for ($i = 0; $i < $metas->length; $i++)
+                    {
+                        $meta = $metas->item($i);
+                        if($meta->getAttribute('property') == 'og:image')
+                            $img_url = $meta->getAttribute('content');
+                    }
+                    // file save
+                    if($img_url !== null) {
+                        $util = new Util();
+                        $resized_image = $util->AzureUploadImage($img_url, $this->channelImagePath);
+
+                        if($resized_image['fileName'] !== null) {
+                            $document['thumbnail_url'] = '/' . $this->channelImagePath . '/' . $resized_image['fileName'];
+                            $document['thumbnail_w'] = $resized_image['width'];
+                            $document['thumbnail_h'] = $resized_image['height'];
+                            $document['ori_thumbnail'] = preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $img_url);
+                            $document['data'] = array(['image' => $document['thumbnail_url']]);
+                            $document['ori_data'] = array($document['thumbnail_url']);
+                        }
+
+                    }
+
+
+
+                    $board = Board::create($document);
+                }
             } //for문
-            $html = $this->file_get_contents_curl($item['originallink']);
-
-            $doc = new \DOMDocument();
-            @$doc->loadHTML($html);
-
-            $metas = $doc->getElementsByTagName('meta');
-
-            $img_url = "";
-            for ($i = 0; $i < $metas->length; $i++)
-            {
-                $meta = $metas->item($i);
-                if($meta->getAttribute('property') == 'og:image')
-                    $img_url = $meta->getAttribute('content');
-            }
-            $img_url = (preg_match('#^http:#', $url) ? $url : str_replace('https:', 'http:', $img_url));
-            $document['ori_thumbnail'] = $img_url;
-
-            // file save
-            $util = new Util();
-            $path = 'images/'.'news'.'/thumbnail/';
-            $resized_image = $util->AzureUploadImage($img_url, $path);
-            //dd($resized_image['fileName']);
-            $image_save = new \stdClass();
-            $image_save->image = "/".$path.$resized_image['fileName'];
-            $data = [$image_save];
-            $document['data'] = $data;
-            $document['thumbnail_url'] = $image_save->image;
-            $document['thumbnail_w'] = (int)$resized_image['width'];
-            $document['thumbnail_h'] = (int)$resized_image['height'];
-            $ori_data = [$resized_image['path']];
-            $document['ori_data'] = $ori_data;
-
-
-            $board = Board::create($document);
-
         }
     }
 
