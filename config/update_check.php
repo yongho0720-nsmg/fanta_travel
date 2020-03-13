d644e74c9f039703efaf5d2a27292891c9bcb1c3
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-03-13 21:03:59 +0900

    앱 버전 up/ 강제 업데이트 설정

--- config/update_check.php
+++ config/update_check.php
@@ -61,12 +61,12 @@ return[
     'fantaholic' =>[
         'version'=>[
             'and'=>[
-                'app_ver' => '1.0.0',   // 앱 최신 버전
-                'forced_update' => 'auto',   // 앱 강제 업데이트 여부 (forced / auto)
+                'app_ver' => '1.1.0',   // 앱 최신 버전
+                'forced_update' => 'forced',   // 앱 강제 업데이트 여부 (forced / auto)
                 'inspection'    =>  false
             ],
             'ios'=>[
-                'app_ver' => '1.0.1',   // 앱 최신 버전
+                'app_ver' => '1.0.0',   // 앱 최신 버전
                 'forced_update' => 'forced',   // 앱 강제 업데이트 여부 (forced / auto)
                 'inspection'    =>  false
             ]
