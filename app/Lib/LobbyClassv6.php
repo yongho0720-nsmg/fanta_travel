f36b19b27e7b8f6544c8140720781a1e20b2b35f
kimseamhee <msmhsi@nsmg21.com>
2020-03-16 15:18:05 +0900

    vlive 검색 조건 변경

--- app/Lib/LobbyClassv6.php
+++ app/Lib/LobbyClassv6.php
@@ -99,7 +99,7 @@ class LobbyClassv6
             })
             ->where('state', 1)
             ->where(function ($query) {
-                $query->where([['post','Not LIKE','%.%'],['type','=','vlive']])->orWhere('type','news');
+               $query->where([['post','Not LIKE','%.%'],['type','!=','vlive']]);
              })
             ->where('data','!=','[]')
             ->orderby('recorded_at', 'desc')
