259acfab47827d711cda0b14805e57fdebdb5ec5
yongho0720-nsmg <yongho@noyonghoui-MacBookPro.local>
2020-03-16 16:51:01 +0900

    앱 버전 수정

--- config/update_check.php
+++ config/update_check.php
@@ -61,7 +61,7 @@ return[
     'fantaholic' =>[
         'version'=>[
             'and'=>[
-                'app_ver' => '1.1.0',   // 앱 최신 버전
+                'app_ver' => '1.2.0',   // 앱 최신 버전
                 'forced_update' => 'forced',   // 앱 강제 업데이트 여부 (forced / auto)
                 'inspection'    =>  false
             ],
